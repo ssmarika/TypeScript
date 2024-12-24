a78cf7c3bec98368331fcaeffa58204cdd042532
ssmarika <smarikashrestha02@gmail.com>
2024-12-24 14:52:36 +0545

    Typescript Basics

---
+++ a.js
@@ -0,0 +1,14 @@
+var x = 5;
+console.log(x);
+// ? create an object called animal which can be either a dog or a cat
+var animal = {
+    name: 'Elelphant',
+    type: 'Herbivorous',
+    weight: 5000,
+    lifeSpan: 60,
+    isPet: false,
+    location: {
+        first: 'Asia',
+        second: 'Africa',
+    },
+};
